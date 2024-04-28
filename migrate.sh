cc7e63406ccf1ce35f328a2ce330e4e30e45f1b4
Zain <zat938@gmail.com>
2024-04-29 02:31:32 +0500

    updates

--- migrate.sh
+++ migrate.sh
@@ -2,7 +2,7 @@
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"zat938@gmail.com"}
 
 # Activate the virtual environment
-. /opt/venv/bin/
+. /opt/venv/bin/activate
 
 python manage.py migrate
 python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
